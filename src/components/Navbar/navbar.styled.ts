aa4204962e4d0186a924a7c6f9b705365f9cdf48
Allan Robson Ferreira <allan.ferreira@dbccompany.com.br>
2023-05-08 18:27:30 -0300

    Ajuste CSS

--- src/components/Navbar/navbar.styled.ts
+++ src/components/Navbar/navbar.styled.ts
@@ -1,10 +1,12 @@
 import styled from 'styled-components'
 
-export const NavContainer = styled.div`
+export const NavContainer = styled.header`
 display: flex;
 width: 100%;
-height: 140px;
+max-width: 100%;
+box-sizing: border-box;
 flex-direction: row;
-
+align-items: center;
+padding: 16px 32px;
 box-shadow: 3px 3px 5px 1px #ccc;
 `
